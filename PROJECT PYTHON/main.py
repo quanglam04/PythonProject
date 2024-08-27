b9dd8cbd469f9379322565e5446e98eb8d31f2d0
datbuudien <nguyenducdatva@gmai.com>
2024-08-28 02:47:46 +0700

    Tôi đã ở đây hơn 24 tiếng và đã tìm ra giải pháp. Lỗi ở đây là do pygame.init() đã không cần nhất thiết phải được gọi, nếu ma co tinh goi o mot vai máy ngu ngu như của tôi thì nó sẽ bị lỗi

--- PROJECT PYTHON/main.py
+++ PROJECT PYTHON/main.py
@@ -2,7 +2,7 @@ import pygame
 from Tank import TankGame
 
 if __name__ == "__main__":
-    pygame.init()
+    #pygame.init()  according to the wiki, pygame.Joystick.init is deprecated (it should have been removed since pygame 2.1, but it seems it didn't get removed
     WINDOW_WIDTH = 1080
     WINDOW_HEIGHT = 720
     window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
